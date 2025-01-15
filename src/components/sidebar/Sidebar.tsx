ba691273c8f91ca014d8a8c886eed838965c096b
Hitomi <122198727+Hitomi-pupila@users.noreply.github.com>
2025-01-16 01:54:09 +0900

    upload files

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,1 +1,48 @@
-create file
+import React from 'react'
+import './Sidebar.scss'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import { SidebarChannel } from './SidebarChannel.tsx';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* sidebarLeft */}
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
+      {/* sidebarRight */}
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+          <ExpandMoreIcon />
+        </div>
+        {/* sidebarChannels */}
+        <div className='sidebarChannels'>
+          <div className='sideberChannelsHeader'>
+            <div className='sidebarHeader'>
+              <ExpandMoreIcon />
+              <h4>プログラミングチャネル</h4>
+            </div>
+            <AddIcon className='sidebarAddIcon'/>
+          </div>
+
+          <div className='sidebarChannelList'>
+            <SidebarChannel />
+          </div>
+
+          <div className='sidebarFooter'>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
